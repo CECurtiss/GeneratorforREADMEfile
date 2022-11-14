cb11259f2520da76223d1e7e32c4ab35177a1995
CECurtiss <curtissclinton@gmail.com>
2022-11-13 19:32:42 -0600

    added user questions for submitting to README

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Enter the Title of your README:",
+            name: 'readmetitle',
+        },
+        {
+            type: 'input',
+            message: "Enter a short description:",
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What are some examples for use of this project?',
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: 'Which type of license are you using?',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+        },
+        {
+            type: 'input',
+            message: 'How can other developers contribute to your project?',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'What tests methods can be performed with your code examples?',
+            name: "tests",
+        },
+        {
+            type: 'input',
+            message: 'Please enter your contact information if you would like to be available for questions from other developers about your project:',
+            name: 'questions'
+        },
+    ])
+const questions = [
+    ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
